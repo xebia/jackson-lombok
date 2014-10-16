e79bb89082a800dbd22edaf431b784811f992a09
Ruben Oostinga <0xR@users.noreply.github.com>
2014-10-16 15:15:07 +0200

    Take JsonProperty annotations on fields into account during serialization and deserialization

--- src/main/java/com/netbeetle/jackson/ConstructorPropertiesAnnotationIntrospector.java
+++ src/main/java/com/netbeetle/jackson/ConstructorPropertiesAnnotationIntrospector.java
@@ -2,22 +2,21 @@ package com.netbeetle.jackson;
 
 import java.beans.ConstructorProperties;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.Collections;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.introspect.Annotated;
 import com.fasterxml.jackson.databind.introspect.AnnotatedConstructor;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
-import com.fasterxml.jackson.databind.introspect.AnnotatedParameter;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
 import com.fasterxml.jackson.databind.introspect.NopAnnotationIntrospector;
+import com.fasterxml.jackson.databind.util.BeanUtil;
 
-public class ConstructorPropertiesAnnotationIntrospector extends NopAnnotationIntrospector
-{
+public class ConstructorPropertiesAnnotationIntrospector extends NopAnnotationIntrospector {
     @Override
-    public boolean hasCreatorAnnotation(Annotated a)
-    {
-        if (!(a instanceof AnnotatedConstructor))
-        {
+    public boolean hasCreatorAnnotation(Annotated a) {
+        if (!(a instanceof AnnotatedConstructor)) {
             return false;
         }
 
@@ -26,25 +25,52 @@ public class ConstructorPropertiesAnnotationIntrospector extends NopAnnotationIn
         Constructor<?> c = ac.getAnnotated();
         ConstructorProperties properties = c.getAnnotation(ConstructorProperties.class);
 
-        if (properties == null)
-        {
+        if (properties == null) {
             return false;
         }
 
-        for (int i = 0; i < ac.getParameterCount(); i++)
-        {
+        for (int i = 0; i < ac.getParameterCount(); i++) {
             String name = properties.value()[i];
+            try {
+                Field field = ac.getDeclaringClass().getDeclaredField(name);
+                if (field != null) {
+                    JsonProperty annotation = field.getAnnotation(JsonProperty.class);
+                    if (annotation != null) {
+                        name = annotation.value();
+                    }
+                }
+            } catch (NoSuchFieldException ignored) {
+            }
             JsonProperty jsonProperty =
-                ProxyAnnotation.of(JsonProperty.class, Collections.singletonMap("value", name));
+                    ProxyAnnotation.of(JsonProperty.class, Collections.singletonMap("value", name));
             ac.getParameter(i).addOrOverride(jsonProperty);
         }
         return true;
     }
 
+
     @Override
     public String findImplicitPropertyName(AnnotatedMember member) {
         JsonProperty property = member.getAnnotation(JsonProperty.class);
+
         if (property == null) {
+            if (member instanceof AnnotatedMethod) {
+                AnnotatedMethod method = (AnnotatedMethod) member;
+                String fieldName = BeanUtil.okNameForGetter(method);
+                try {
+                    if (fieldName != null) {
+                        Field field = member.getDeclaringClass().getDeclaredField(fieldName);
+                        if (field != null) {
+                            JsonProperty fieldProperty = field.getAnnotation(JsonProperty.class);
+                            if (fieldProperty != null) {
+                                return fieldProperty.value();
+                            }
+                        }
+                    }
+                } catch (NoSuchFieldException ignored) {
+                }
+
+            }
             return null;
         }
         return property.value();
--- src/test/java/com/netbeetle/jackson/ConstructorPropertiesAnnotationIntrospectorTest.java
+++ src/test/java/com/netbeetle/jackson/ConstructorPropertiesAnnotationIntrospectorTest.java
@@ -5,14 +5,29 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Value;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ConstructorPropertiesAnnotationIntrospectorTest {
+
+    public static final String JSON = "{\"new_name\":\"foobar\",\"value\":42}";
+    public static final String INVALID_JSON = "{\"name\":\"foobar\",\"value\":42}";
+    private ObjectMapper mapperWithExtention;
+
+    @Before
+    public void setUp() {
+        mapperWithExtention = new ObjectMapper();
+        mapperWithExtention.setAnnotationIntrospector(new ConstructorPropertiesAnnotationIntrospector());
+    }
+
     @Value
     private static class ImmutablePojo {
+        @JsonProperty("new_name")
         String name;
         int value;
     }
@@ -26,13 +41,22 @@ public class ConstructorPropertiesAnnotationIntrospectorTest {
         mapper.readValue(json, ImmutablePojo.class);
     }
 
+
+    @Test
+    public void testJacksonAbleToSerialize() throws IOException {
+        String json = mapperWithExtention.writeValueAsString(instance);
+        assertThat(json, is(JSON));
+    }
+
     @Test
     public void testJacksonAbleToDeserialize() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setAnnotationIntrospector(new ConstructorPropertiesAnnotationIntrospector());
-        String json = mapper.writeValueAsString(instance);
-        assertThat(json, is("{\"name\":\"foobar\",\"value\":42}"));
-        ImmutablePojo output = mapper.readValue(json, ImmutablePojo.class);
+        ImmutablePojo output = mapperWithExtention.readValue(JSON, ImmutablePojo.class);
+        assertThat(output, is(instance));
+    }
+
+    @Test(expected = JsonMappingException.class)
+    public void testJacksonUnableToDeserializeInvalidJson() throws IOException {
+        ImmutablePojo output = mapperWithExtention.readValue(INVALID_JSON, ImmutablePojo.class);
         assertThat(output, is(instance));
     }
 }
